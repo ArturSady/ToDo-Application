04ce7a6e8ff632f499b32261c18f0182e0fd229e
Artur <asady502@gmail.com>
2023-08-02 18:00:57 +0200

    implements RepositoryrestConfigurer add @NotBlank above String "description"

--- src/main/java/com/ArturSady/todoapp/model/Task.java
+++ src/main/java/com/ArturSady/todoapp/model/Task.java
@@ -1,13 +1,44 @@
 package com.ArturSady.todoapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
     private boolean done;
 
+    public Task() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
